c35dc7c155f1a6f907114e58853c9eb031b7ac08
key-on423 <@Zaqxsw951654>
2024-11-24 18:25:42 +0800

    002

---
+++ Python1009/Python1009.py
@@ -0,0 +1,63 @@
+﻿
+
+
+
+# x=input("请输入:")
+# x=int(x)
+# sum=0
+# k=1
+# for i in range(1,x+1):
+#     k*=i
+#     sum+=k
+    
+# print("sum=%i" %sum)
+
+# x=input("请输入:")
+# x=int(x)
+# sum=0
+# k=1
+# for i in range(1,x+1):
+#     k*=i
+# sum+=k
+    
+# print("sum=%i" %sum)
+
+
+
+
+# yw=int(input("请输入语文成绩："))
+# sx=int(input("请输入数学成绩："))
+# yy=int(input("请输入英语成绩："))
+# if yw>=60 and sx>=60 and yy >=60:
+#     print("三门课程都及格")
+# if yw>=60 or sx>=60 or yy>=60:
+#             print("至少一门课程及格")
+# if yw>=60 and((sx>=90 and yy<90)or(sx<90 and yy>=90)):
+#               print("语文及格且数学或英语优秀")
+
+
+# def nihao():
+#     print("haha")
+#     pass
+
+# x=int(input("请输入你的成绩"))
+# if x>90:
+#     print("very good!")
+# elif x>60:
+#     pass
+# else :
+#     print("bad")
+
+# print("结束")
+
+
+# print("* * * * * * * * * * * * * * " )
+# print("*   Hello,Python Program   *")
+# print("* * * * * * * * * * * * * * " )
+
+
+sum=0
+for i in range(100):
+    if i%2!=0:
+        sum+=i
+print("1到100的奇数和为",sum)
